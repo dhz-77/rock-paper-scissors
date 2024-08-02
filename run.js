299f4f54f742fdb722d9ef340967f0fb192a98e2
Dominik Santoro <174945216+dhz-77@users.noreply.github.com>
2024-08-02 13:31:32 +0200

    Remove pseudo-code and redundant comments

--- run.js
+++ run.js
@@ -3,50 +3,30 @@
 console.log("Test HTML to JS link: OK");
 
 function getComputerChoice() {
-    // Math.random() returns a random number equal to 0 and less than 1
+
     function getRandomFloat() {
         return Math.random();
     }
 
-    // This value should not change within a run, so we can check that our 
-    // main algorithm further below takes this exact float number and works 
-    // as intended
-    const randomFloat =  getRandomFloat(); 
+    const randomFloat =  getRandomFloat();
     console.log("Computer random float: " + randomFloat); 
-    // For example: returns 0.55
-
-    // main algorithm:
-
-    // Create 3 different sections between 0 and 1, each representing 
-    // "rock", "paper" or "scissors" (0-1/3), (1/3 - 2/3), (2/3 - 3/3) 
-    // (but excluding 3/3)
-   
-    // Create an if statement for each section and corresponding string
-    // and return "rock", "paper" or "scissors"
-
-    // Use the constant "randomFloat" number (e.g. 0.55) from above, and do not 
-    // call the getRandomFloat() function again, otherwise a new, different number 
-    // would be generated and we could not justify if the code works as intended
     
     if (randomFloat >= 0 && randomFloat < (1/3)) {
         return "rock";
     }
     else if (randomFloat >= 1/3 && randomFloat < (2/3)){
         return "paper";
-    } else { // implicitly means >= 2/3 && < 3/3
+    } else { // implicitly >= 2/3 && < 3/3
         return "scissors";
     }
 
 }
 
 function getHumanChoice() {
-    // write a function that takes the user choice and returns it
 
-    // create a pop up window which prompts the user to input text and
-    // return a valid choice
     const humanChoice = prompt("Rock, paper or scissors?");
 
-    if (   humanChoice === null // when human player clicks "cancel" in the prompt window
+    if (   humanChoice === null // player clicks "cancel"
         || humanChoice.toLowerCase() === "rock"
         || humanChoice.toLowerCase() === "paper"
         || humanChoice.toLowerCase() === "scissors") {
@@ -58,13 +38,8 @@ function getHumanChoice() {
 
 console.log(playGame());
 
-// The game will play 5 rounds. Write a function that calls playRound 
-// to play 5 rounds, keeps track of the scores and declares a winner 
-// at the end.
-
-function playGame() {
-    // Keep track of players score in global scope
-    // Value to be reassigned over time, so we use "let":
+function playGame() { // play 5 rounds, keep track of scores, declare winner
+    
     let humanScore = 0;
     let computerScore = 0;
     let humanSelection;
@@ -109,9 +84,6 @@ function playGame() {
         }
     }
 
-    // write a function that takes the human and computer player choices
-    // as arguments, plays a single round, increments the round winner’s 
-    // score and logs a winner announcement.
     function playRound(humanChoice, computerChoice) {
         const hChoice = humanChoice.toLowerCase();
 
